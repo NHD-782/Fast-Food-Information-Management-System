f3e21e143a496dfcdc610bea9d2cd300661968b0
NHD-782 <125521102+NHD-782@users.noreply.github.com>
2023-04-04 21:45:05 +0700

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,5 @@
+# User Credentials
+host = 'localhost'
+user = 'root'
+password = '07082002'
+database = 'mydatabase'
---
+++ custom.py
@@ -0,0 +1,7 @@
+# Color and Font
+color_1 = "#4a536b"
+color_2 = "#aed6dc"
+color_3 = "black"
+color_4 = "white"
+font_1 = "times new roman"
+font_2 = "helvetica"
